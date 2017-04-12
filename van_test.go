714f99db9f4dadda60876eaeba379b55ae62d323
Andy Brown <andybrown668@gmail.com>
2017-04-12 14:13:35 +0100

    add tests

---
+++ van_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/d2r2/go-dht"
+	"testing"
+)
+
+func TestDHT(t *testing.T) {
+	tmp, h, err := dht.ReadDHTxx(dht.DHT22, 10, true)
+	fmt.Println(tmp, h, err)
+}
+
+func BenchDHT(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		tmp, h, err := dht.ReadDHTxx(dht.DHT22, 10, true)
+		fmt.Println(tmp, h, err)
+	}
+}
