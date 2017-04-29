8080bf3ca31317b393da02d49031dfd2e05dae58
Andy Brown <andybrown668@gmail.com>
2017-04-29 09:10:50 +0100

    basic motion detection recording from webcam

--- van.go
+++ van.go
@@ -1,5 +1,8 @@
 package main
 
+// #include "go_sched.h"
+import "C"
+
 import (
 	"fmt"
 	"image"
@@ -14,11 +17,20 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/d2r2/go-dht"
-	"github.com/golang/freetype"
+	"image/color"
 	"image/png"
 	"math/rand"
-	"image/color"
+
+	"bytes"
+	"image/jpeg"
+	"os"
+
+	"github.com/blackjack/webcam"
+	"github.com/d2r2/go-dht"
+	"github.com/golang/freetype"
+	"sync"
+	"github.com/golang/freetype/truetype"
+	"io"
 )
 
 type Stats struct {
@@ -59,10 +71,11 @@ var (
 )
 
 func main() {
-	monitorWifi()
-	monitorDHT()
-	monitorBatteries()
-	monitorAdc()
+	go monitorWebcam()
+	//monitorWifi()
+	//monitorDHT()
+	//monitorBatteries()
+	//monitorAdc()
 	startHttpApi()
 
 	if err := initialize(); err != nil {
@@ -87,7 +100,9 @@ func main() {
 		lines := []string{
 			title,
 		}
-		display(lines)
+		if err := display(lines); err != nil {
+			fmt.Println("failed to display:", err)
+		}
 		time.Sleep(500 * time.Millisecond)
 	}
 }
@@ -107,11 +122,255 @@ func startHttpApi() {
 		w.Header().Set("Content-Type", "image/png")
 		png.Encode(w, dst)
 	})
+	//webcam
+	http.HandleFunc("/webcamraw", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/png")
+		w.Header().Set("Refresh", "15;url=webcamraw")
+		mu.Lock()
+		defer mu.Unlock()
+		//if frame1 != nil {
+		//	png.Encode(w, frame1)
+		//}
+		if file, err := os.Open("now.jpg"); err == nil {
+			io.Copy(w, file)
+			file.Close()
+		} else {
+			fmt.Println("failed to read now.jpg:", err)
+		}
+	})
+	http.HandleFunc("/webcam", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/png")
+		w.Header().Set("Refresh", "15;url=webcam")
+		mu.Lock()
+		defer mu.Unlock()
+		if gray3 != nil {
+			png.Encode(w, gray3)
+		}
+	})
 	go func() {
 		if err := http.ListenAndServe(":8080", nil); err != nil {
 			fmt.Println(err)
 		}
 	}()
+}
+
+var (
+	dhtMarker = []byte{255, 196}
+	dhtable   = []byte{1, 162, 0, 0, 1, 5, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 0, 2, 1, 3, 3, 2, 4, 3, 5, 5, 4, 4, 0, 0, 1, 125, 1, 2, 3, 0, 4, 17, 5, 18, 33, 49, 65, 6, 19, 81, 97, 7, 34, 113, 20, 50, 129, 145, 161, 8, 35, 66, 177, 193, 21, 82, 209, 240, 36, 51, 98, 114, 130, 9, 10, 22, 23, 24, 25, 26, 37, 38, 39, 40, 41, 42, 52, 53, 54, 55, 56, 57, 58, 67, 68, 69, 70, 71, 72, 73, 74, 83, 84, 85, 86, 87, 88, 89, 90, 99, 100, 101, 102, 103, 104, 105, 106, 115, 116, 117, 118, 119, 120, 121, 122, 131, 132, 133, 134, 135, 136, 137, 138, 146, 147, 148, 149, 150, 151, 152, 153, 154, 162, 163, 164, 165, 166, 167, 168, 169, 170, 178, 179, 180, 181, 182, 183, 184, 185, 186, 194, 195, 196, 197, 198, 199, 200, 201, 202, 210, 211, 212, 213, 214, 215, 216, 217, 218, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 17, 0, 2, 1, 2, 4, 4, 3, 4, 7, 5, 4, 4, 0, 1, 2, 119, 0, 1, 2, 3, 17, 4, 5, 33, 49, 6, 18, 65, 81, 7, 97, 113, 19, 34, 50, 129, 8, 20, 66, 145, 161, 177, 193, 9, 35, 51, 82, 240, 21, 98, 114, 209, 10, 22, 36, 52, 225, 37, 241, 23, 24, 25, 26, 38, 39, 40, 41, 42, 53, 54, 55, 56, 57, 58, 67, 68, 69, 70, 71, 72, 73, 74, 83, 84, 85, 86, 87, 88, 89, 90, 99, 100, 101, 102, 103, 104, 105, 106, 115, 116, 117, 118, 119, 120, 121, 122, 130, 131, 132, 133, 134, 135, 136, 137, 138, 146, 147, 148, 149, 150, 151, 152, 153, 154, 162, 163, 164, 165, 166, 167, 168, 169, 170, 178, 179, 180, 181, 182, 183, 184, 185, 186, 194, 195, 196, 197, 198, 199, 200, 201, 202, 210, 211, 212, 213, 214, 215, 216, 217, 218, 226, 227, 228, 229, 230, 231, 232, 233, 234, 242, 243, 244, 245, 246, 247, 248, 249, 250}
+	sosMarker = []byte{255, 218}
+)
+
+func addMotionDht(frame []byte) []byte {
+	jpegParts := bytes.Split(frame, sosMarker)
+	if len(jpegParts) < 2 {
+		return frame
+	}
+	return append(jpegParts[0], append(dhtMarker, append(dhtable, append(sosMarker, jpegParts[1]...)...)...)...)
+}
+
+func downsampleFile(filename string) (gray *image.Gray, err error) {
+	if imgData, err := ioutil.ReadFile(filename); err != nil {
+		return nil, err
+	} else if img, err := jpeg.Decode(bytes.NewReader(addMotionDht(imgData))); err != nil {
+		return nil, err
+	} else {
+		return downsampleImage(img), nil
+	}
+}
+
+func downsampleImage(img image.Image) (gray *image.Gray) {
+	//downsampleFile all pixels to grayscale
+	bounds := img.Bounds()
+	gray = image.NewGray(bounds)
+	draw.Draw(gray, bounds, &image.Uniform{color.Black}, image.ZP, draw.Src)
+
+	for y := 0; y < bounds.Dy(); y += 1 {
+		for x := 0; x < bounds.Dx(); x += 1 {
+			r, _, _, _ := img.At(x, y).RGBA()
+			//use red channel only
+			r = r >> 8
+			bw := color.Gray{uint8(r)}
+			gray.SetGray(x, y, bw)
+		}
+	}
+	return gray
+}
+
+//subtract one image from another
+func diff(image1, image2 *image.Gray) (gray *image.Gray, changes int) {
+	//downsampleFile all pixels to grayscale
+	bounds := image1.Bounds()
+	gray = image.NewGray(bounds)
+	draw.Draw(gray, bounds, &image.Uniform{color.White}, image.ZP, draw.Src)
+
+	total := bounds.Dx() * bounds.Dy()
+	for y := 0; y < bounds.Dy(); y++ {
+		for x := 0; x < bounds.Dx(); x++ {
+			c1 := image1.GrayAt(x, y)
+			c2 := image2.GrayAt(x, y)
+			r1 := c1.Y
+			r2 := c2.Y - r1
+			if r2 < 0 {
+				r2 = 0 - r2
+			}
+
+			if r2 > 60 && r2 < 200 {
+				r2 = 255
+				changes++
+			} else {
+				r2 = 0
+			}
+			gray.Set(x, y, color.Gray{r2})
+		}
+	}
+
+	return gray, (10000 * changes) / total
+}
+
+func saveImage(img *image.Gray, filename string) error {
+	// copy the image as rgb so we can annotate it and add a timestamp/info strip below
+	bounds := img.Bounds()
+	bounds.Max.Y += 20
+	i := image.NewRGBA(bounds)
+	draw.Draw(i, bounds, img, image.ZP, draw.Src)
+
+	// make the info strip white
+	strip := bounds
+	strip.Min.Y = strip.Max.Y - 20
+	draw.Draw(i, strip, &image.Uniform{color.White}, image.ZP, draw.Src)
+	c := freetype.NewContext()
+	c.SetDst(i)
+	c.SetClip(i.Bounds())
+	c.SetSrc(image.NewUniform(color.RGBA{R:0x00, B:0xff, A:0xff}))
+	c.SetFont(fnt)
+	c.SetFontSize(18)
+	if _, err := c.DrawString(time.Now().Format("2006-01-02 15:04:05"), freetype.Pt(5, bounds.Max.Y-4)); err != nil {
+		return err
+	}
+	if f, err := os.Create(filename); err != nil {
+		return err
+	} else {
+		jpeg.Encode(f, i, &jpeg.Options{Quality: 100})
+	}
+	return nil
+}
+
+var mu sync.Mutex
+var frame1, gray3 *image.Gray
+
+//get a downsampled frame or nil on error
+func getFrame(cam *webcam.Webcam) *image.Gray {
+
+	if err := cam.WaitForFrame(5); err != nil {
+		fmt.Println("Failed waiting for frame", err)
+		return nil
+	}
+
+	var img image.Image
+
+	if frame, err := cam.ReadFrame(); err != nil {
+		fmt.Println("failed to get frame", err)
+	} else if img, err = jpeg.Decode(bytes.NewReader(addMotionDht(frame))); err != nil {
+		fmt.Println("failed to decode frame", err)
+	}
+
+	if img != nil {
+		return downsampleImage(img)
+	}
+
+	return nil
+}
+
+func monitorWebcam() {
+	cam, err := webcam.Open("/dev/video0")
+	if err != nil {
+		fmt.Println("failed to open webcam", err)
+	}
+	defer cam.Close()
+
+	var format webcam.PixelFormat
+	formats := cam.GetSupportedFormats()
+	for k, v := range formats {
+		fmt.Println(k, v)
+		if v == "Motion-JPEG" {
+			format = k
+		}
+	}
+
+	sizes := cam.GetSupportedFrameSizes(format)
+	fmt.Println(sizes)
+	var size webcam.FrameSize
+	size = sizes[1]
+	size.MaxWidth = 320
+	size.MaxHeight = 240
+	if _, _, _, err := cam.SetImageFormat(format, size.MaxWidth, size.MaxHeight); err != nil {
+		fmt.Println("failed to set format and size: ", err)
+	}
+
+	n := 0
+
+	//get and compare consecutive frames
+	var frame2 *image.Gray
+	for {
+		time.Sleep(250 * time.Millisecond)
+		if n == 0 {
+			if err := cam.StartStreaming(); err != nil {
+				fmt.Println("error starting streaming:", err)
+				break
+			}
+
+		}
+		//read frame 1 or use prior frame2
+		if frame2 == nil {
+			if frame := getFrame(cam); frame == nil {
+				continue
+			} else {
+				mu.Lock()
+				frame1 = frame
+				mu.Unlock()
+			}
+		} else {
+			mu.Lock()
+			frame1 = frame2
+			mu.Unlock()
+		}
+
+		//always save current frame
+		mu.Lock()
+		if err := saveImage(frame1, "now.jpg"); err != nil {
+			fmt.Println("error saving current:", err)
+		}
+		mu.Unlock()
+
+		if frame2 = getFrame(cam); frame2 == nil {
+			continue
+		}
+
+		//subtract one image from another
+		mu.Lock()
+		var changes int
+		gray3, changes = diff(frame1, frame2)
+		mu.Unlock()
+		if changes < 50 {
+			continue
+		}
+
+		//save diff frame and two input frames
+		if err := saveImage(frame1, fmt.Sprintf("./%d-before-%d.jpg", n, changes)); err != nil {
+			fmt.Println("failed to write frame 1")
+		}
+		if err := saveImage(frame2, fmt.Sprintf("./%d-after-%d.jpg", n, changes)); err != nil {
+			fmt.Println("failed to write frame 2")
+		}
+		if err := saveImage(gray3, fmt.Sprintf("./%d-changes%d.jpg", n, changes)); err != nil {
+			fmt.Println("failed to write frame diff")
+		} else {
+			fmt.Println("motion at", n)
+			n++
+		}
+
+	}
+
 }
 
 //monitor connectivity state regularly
@@ -131,10 +390,31 @@ func monitorWifi() {
 }
 
 //monitor temp and humdidty regularly
+type Reading struct {
+	value    C.int
+	duration time.Duration
+	ticks    uint32
+}
+
+func (this Reading) String() string {
+	//return fmt.Sprintf("%c %s,", this.value, this.duration)
+	if this.value == 38 {
+		return "."
+	} else if this.value == 39 {
+		if this.ticks == 3 {
+			return "1"
+		} else {
+			return "0"
+		}
+	} else {
+		return fmt.Sprintf("%c", this.value)
+	}
+}
+
 func monitorDHT() {
 	go func() {
 		for {
-			t, h, err := dht.ReadDHTxx(dht.DHT22, 10, false)
+			t, h, err := dht.ReadDHTxx(dht.DHT22, 10, true)
 			if err == nil {
 				stats.Temperature = int(t)
 				stats.Humidity = int(h)
@@ -175,30 +455,30 @@ func monitorAdc() {
 		//open the adc
 		d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x48)
 		if err != nil {
-			fmt.Println("open error", err)
+			fmt.Println("adc open error", err)
 		}
 		defer d.Close()
 
 		// write to control register
-		if err = d.Write([]byte{TWO_DIFF | AIN0}); err != nil {
-			fmt.Println("write error", err)
+		if err = d.Write([]byte{FOUR_SINGLE | AIN0}); err != nil {
+			fmt.Println("adc write error", err)
 		}
-		twos := func(b byte) int8 {
-			if b>>7 == 0 {
-				return int8(b)
-			} else {
-				b = b - (1 << 7)
-				return int8(b) - 127 - 1
-			}
-		}
+		//twos := func(b byte) int8 {
+		//	if b>>7 == 0 {
+		//		return int8(b)
+		//	} else {
+		//		b = b - (1 << 7)
+		//		return int8(b) - 127 - 1
+		//	}
+		//}
 		read := make([]byte, 0x1)
 		for {
 			//request to read
 			if err = d.Read(read); err != nil {
-				fmt.Println("read error", err)
+				fmt.Println("adc read error", err)
 			} else {
 				//convert two's complement
-				n := twos(read[0])
+				n := read[0] //twos(read[0])
 				fmt.Print(n, " ")
 			}
 			time.Sleep(500 * time.Millisecond)
@@ -225,18 +505,21 @@ func monitorBatteries() {
 	}()
 }
 
+var fnt *truetype.Font
 func initialize() (err error) {
 	oled, err = monochromeoled.Open(&i2c.Devfs{Dev: "/dev/i2c-1"})
 	if err == nil {
 		oled.On()
 		oled.Clear()
+	} else {
+		fmt.Println("failed to open display")
 	}
 
 	data, err := ioutil.ReadFile("./enhanced_dot_digital-7.ttf")
 	if err != nil {
 		return err
 	}
-	f, err := freetype.ParseFont(data)
+	fnt, err = freetype.ParseFont(data)
 	if err != nil {
 		return err
 	}
@@ -244,7 +527,7 @@ func initialize() (err error) {
 	c.SetDst(dst)
 	c.SetClip(dst.Bounds())
 	c.SetSrc(image.Black)
-	c.SetFont(f)
+	c.SetFont(fnt)
 
 	return nil
 }
@@ -279,8 +562,8 @@ func display(lines []string) (err error) {
 
 		//charge meter
 		for x := 16; x <= int(80*charge); x += 4 {
-			for dx := 0; dx < 3; dx ++ {
-				for dy := 0; dy < 3; dy ++ {
+			for dx := 0; dx < 3; dx++ {
+				for dy := 0; dy < 3; dy++ {
 					dst.Set(x+dx, y+dy, color.Black)
 				}
 			}
--- van_test.go
+++ van_test.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"image"
-	"image/color"
-	"image/draw"
 	"image/jpeg"
-	"io/ioutil"
 	"testing"
 
-	"github.com/d2r2/go-dht"
 	"os"
+
+	"github.com/d2r2/go-dht"
 )
 
 func TestDHT(t *testing.T) {
@@ -26,99 +23,44 @@ func BenchDHT(b *testing.B) {
 	}
 }
 
-var (
-	dhtMarker = []byte{255, 196}
-	dhtable   = []byte{1, 162, 0, 0, 1, 5, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 0, 2, 1, 3, 3, 2, 4, 3, 5, 5, 4, 4, 0, 0, 1, 125, 1, 2, 3, 0, 4, 17, 5, 18, 33, 49, 65, 6, 19, 81, 97, 7, 34, 113, 20, 50, 129, 145, 161, 8, 35, 66, 177, 193, 21, 82, 209, 240, 36, 51, 98, 114, 130, 9, 10, 22, 23, 24, 25, 26, 37, 38, 39, 40, 41, 42, 52, 53, 54, 55, 56, 57, 58, 67, 68, 69, 70, 71, 72, 73, 74, 83, 84, 85, 86, 87, 88, 89, 90, 99, 100, 101, 102, 103, 104, 105, 106, 115, 116, 117, 118, 119, 120, 121, 122, 131, 132, 133, 134, 135, 136, 137, 138, 146, 147, 148, 149, 150, 151, 152, 153, 154, 162, 163, 164, 165, 166, 167, 168, 169, 170, 178, 179, 180, 181, 182, 183, 184, 185, 186, 194, 195, 196, 197, 198, 199, 200, 201, 202, 210, 211, 212, 213, 214, 215, 216, 217, 218, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 17, 0, 2, 1, 2, 4, 4, 3, 4, 7, 5, 4, 4, 0, 1, 2, 119, 0, 1, 2, 3, 17, 4, 5, 33, 49, 6, 18, 65, 81, 7, 97, 113, 19, 34, 50, 129, 8, 20, 66, 145, 161, 177, 193, 9, 35, 51, 82, 240, 21, 98, 114, 209, 10, 22, 36, 52, 225, 37, 241, 23, 24, 25, 26, 38, 39, 40, 41, 42, 53, 54, 55, 56, 57, 58, 67, 68, 69, 70, 71, 72, 73, 74, 83, 84, 85, 86, 87, 88, 89, 90, 99, 100, 101, 102, 103, 104, 105, 106, 115, 116, 117, 118, 119, 120, 121, 122, 130, 131, 132, 133, 134, 135, 136, 137, 138, 146, 147, 148, 149, 150, 151, 152, 153, 154, 162, 163, 164, 165, 166, 167, 168, 169, 170, 178, 179, 180, 181, 182, 183, 184, 185, 186, 194, 195, 196, 197, 198, 199, 200, 201, 202, 210, 211, 212, 213, 214, 215, 216, 217, 218, 226, 227, 228, 229, 230, 231, 232, 233, 234, 242, 243, 244, 245, 246, 247, 248, 249, 250}
-	sosMarker = []byte{255, 218}
-)
-
-func addMotionDht(frame []byte) []byte {
-	jpegParts := bytes.Split(frame, sosMarker)
-	return append(jpegParts[0], append(dhtMarker, append(dhtable, append(sosMarker, jpegParts[1]...)...)...)...)
-}
-
-func downsample(filename string) (gray *image.Gray, err error) {
-	if imgData, err := ioutil.ReadFile(filename); err != nil {
-		return nil, err
-	} else if img, err := jpeg.Decode(bytes.NewReader(addMotionDht(imgData))); err != nil {
-		return nil, err
-	} else {
-		//downsample all pixels to grayscale
-		bounds := img.Bounds()
-		gray = image.NewGray(bounds)
-		draw.Draw(gray, bounds, &image.Uniform{color.Black}, image.ZP, draw.Src)
-
-		for y := 0; y < bounds.Dy(); y++ {
-			for x := 0; x < bounds.Dx(); x++ {
-				r, _, _, _ := img.At(x, y).RGBA()
-				//use red channel only
-				r = r >> 8
-				bw := color.Gray{uint8(r)}
-				gray.SetGray(x, y, bw)
-			}
-		}
-		return gray, nil
-	}
-}
-
-//subtract one image from another
-func diff(image1, image2 *image.Gray) (gray *image.Gray, changes int) {
-	//downsample all pixels to grayscale
-	bounds := image1.Bounds()
-	gray = image.NewGray(bounds)
-	draw.Draw(gray, bounds, &image.Uniform{color.White}, image.ZP, draw.Src)
-
-	total := bounds.Dx() * bounds.Dy()
-	for y := 0; y < bounds.Dy(); y++ {
-		for x := 0; x < bounds.Dx(); x++ {
-			c1 := image1.GrayAt(x, y)
-			c2 := image2.GrayAt(x, y)
-			r1 := c1.Y
-			r2 := c2.Y - r1
-			if r2 < 0 {
-				r2 = 0 - r2
-			}
-
-			if r2 > 60 && r2 < 200 {
-				r2 = 255
-				changes++
-			} else {
-				r2 = 0
-			}
-			gray.Set(x, y, color.Gray{r2})
-		}
-	}
-
-	fmt.Println(total, changes)
-	return gray, (100 * changes) / total
-}
-
 func TestSceneCompaction(t *testing.T) {
 	var gray, gray2 *image.Gray
 	var err error
 
-	if gray, err = downsample("frame-1.jpg"); err != nil {
-		t.Fatal(err)
-	} else if out, err := os.Create("./output.jpg"); err != nil {
-		t.Fatal(err)
-	} else {
-		jpeg.Encode(out, gray, &jpeg.Options{Quality: 100})
-	}
+	for frame := 1; frame < 67; frame++ {
+		//load first frame or copy from prior second frame
+		if gray2 == nil {
+			if gray, err = downsampleFile(fmt.Sprintf("frame-%d.jpg", frame-1)); err != nil {
+				t.Fatal(err)
+			}
+		} else {
+			gray = gray2
+		}
+		if gray2, err = downsampleFile(fmt.Sprintf("frame-%d.jpg", frame)); err != nil {
+			t.Fatal(err)
+		}
 
-	if gray2, err = downsample("frame-2.jpg"); err != nil {
-		t.Fatal(err)
-	} else if out, err := os.Create("./output2.jpg"); err != nil {
-		t.Fatal(err)
-	} else {
-		jpeg.Encode(out, gray2, &jpeg.Options{Quality: 100})
-	}
-
-	//subtract one image from another
-	if out2, err := os.Create("./output3.jpg"); err != nil {
-		t.Fatal(err)
-	} else {
+		//subtract one image from another
 		gray3, changes := diff(gray, gray2)
-		jpeg.Encode(out2, gray3, &jpeg.Options{Quality: 100})
-		fmt.Println(changes)
+		if changes < 50 {
+			continue
+		}
+		if out2, err := os.Create(fmt.Sprintf("./output-%d-%d.jpg", frame, changes)); err != nil {
+			t.Fatal(err)
+		} else {
+			jpeg.Encode(out2, gray3, &jpeg.Options{Quality: 100})
+		}
+	}
+
+}
+
+func TestImageAnnotation(t *testing.T) {
+	initialize()
+	img, err := downsampleFile("./frame2-37-624.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := saveImage(img, "./result.jpg"); err != nil{
+		t.Fatal(err)
 	}
 }
