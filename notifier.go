2c021fea6d90ff36c3ca14c6fd6fcf5526842b76
Andy Brown <andybrown668@gmail.com>
2017-04-30 16:09:17 +0100

    clean up old slack messages on startup

--- notifier.go
+++ notifier.go
@@ -10,12 +10,16 @@ import (
 var slackApi *slack.Client
 var token = os.Getenv("SLACK_API_TOKEN")
 var enabled = true
+
+const Channel = "camera"
+
 func notify(text string) {
 	if slackApi == nil {
 		if token == "" {
 			enabled = false
 		} else {
 			slackApi = slack.New(token)
+			cleanup()
 		}
 	}
 
@@ -24,10 +28,39 @@ func notify(text string) {
 		return
 	}
 
-	channel, time, err := slackApi.PostMessage("camera", text, slack.PostMessageParameters{})
+	channel, time, err := slackApi.PostMessage(Channel, text, slack.PostMessageParameters{})
 	if err == nil {
 		fmt.Printf("sent to %s at %s\n", channel, time)
 	} else {
 		fmt.Printf("failed to send: %s\n", err)
 	}
+}
+
+func cleanup() {
+	//remove all old messages
+	channelId := ""
+	if channels, err := slackApi.GetChannels(true); err != nil {
+		fmt.Printf("failed to get channel: %s\n", err)
+		return
+	} else {
+		for _, channel := range channels {
+			if channel.Name == Channel {
+				channelId = channel.ID
+			}
+		}
+	}
+
+	if history, err := slackApi.GetChannelHistory(channelId, slack.NewHistoryParameters()); err != nil {
+		fmt.Printf("failed to get history: %s\n", err)
+	} else {
+		for _, message := range history.Messages {
+			if _, _, err := slackApi.DeleteMessage(channelId, message.Timestamp); err != nil {
+				fmt.Printf("failed to delete message: %s\n", err)
+				break
+			} else {
+				fmt.Printf("Deleted msg from %s", message.Timestamp)
+			}
+		}
+	}
+
 }
